f0f7e1ffbee462c55941e46629befe7228593381
uncannysmile <sprinklingsunshower@gmail.com>
2023-03-27 12:13:13 -0700

    most of oAuth implemented

--- hypin-app/controllers/artists.js
+++ hypin-app/controllers/artists.js
@@ -6,3 +6,15 @@ const Release = require('../models/release');
 module.exports = {
     new: newArtist,
 };
+
+passport.serializeUser(function(user, cb) {
+    cb(null, user._id);
+  });
+
+  passport.deserializeUser(async function(userId, cb) {
+    cb(null, await User.findById(userId));
+    // The above async/await code replaces this code
+    // User.findById(userId).then(function(user) {
+    //   cb(null, user);
+    // });
+  });
--- hypin-app/routes/index.js
+++ hypin-app/routes/index.js
@@ -1,11 +1,41 @@
 const express = require('express');
 const router = express.Router();
-// Allows for Roger to use fetch:
+const passport = require('passport');
+// Allows Legacy users to use fetch:
 const fetch = require('node-fetch');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+// Google OAuth login route
+router.get('/auth/google', passport.authenticate(
+  // Which passport strategy is being used?
+  'google',
+  {
+    // Requesting the user's profile and email
+    scope: ['profile', 'email'],
+    // Optionally force pick account every time
+    // prompt: "select_account"
+  }
+));
+
+// Google OAuth callback route
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/',
+    failureRedirect: '/'
+  }
+));
+
+// OAuth logout route
+router.get('/logout', function (req, res) {
+  req.logout(function () {
+    res.redirect('/');
+  });
+});
+
 
 module.exports = router;
