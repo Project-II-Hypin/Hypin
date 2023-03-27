8f090780280677cfea2f82ffc54bb2d46bdc4102
redroguea1 <macdickerson@gmail.com>
2023-03-27 14:22:09 -0700

    building Artist Scheme and the artist show page

--- hypin-app/models/artist.js
+++ hypin-app/models/artist.js
@@ -2,7 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const artistSchema = new Schema({
-
+    artistId: {
+        type: Number,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    }
 });
 
 module.exports = mongoose.model('Artist', artistSchema);
