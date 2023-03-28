ca4979513b0b3e172fad58a8fb14a2cb531b03b3
V3nonix <liam.grubbs@outlook.com>
2023-03-28 11:39:58 -0700

    Removed code.

--- hypin-app/routes/artists.js
+++ hypin-app/routes/artists.js
@@ -6,8 +6,7 @@ const artistsCtrl = require('../controllers/artists');
 	
 // GET /artists/new
 router.get('/new', artistsCtrl.new);
-// GET /artist/releases/
-router.get('/new',)
+
 // GET /artist/releases/:id (show functionality) MUST be below new route
 
 // POST /artists/new
