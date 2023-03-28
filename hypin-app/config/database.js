12a38cb6b04a6db026a1ff684c1657a4903caa4a
redroguea1 <macdickerson@gmail.com>
2023-03-27 18:16:59 -0700

    adding code to the models

--- hypin-app/config/database.js
+++ hypin-app/config/database.js
@@ -2,6 +2,13 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.DATABASE_URL);
 
+mongoose.connect(/*DATABASE_URL,*/ { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+}, () => { 
+    console.log('connected to database myDb ;)') 
+})
+
 const db = mongoose.connection;
 
 db.on('connected', function() {
