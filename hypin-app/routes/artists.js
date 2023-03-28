ca79b8aea29c998caa43575b6a55ace0e8fe5ce5
uncannysmile <sprinklingsunshower@gmail.com>
2023-03-28 11:56:07 -0700

    deleted dupes

--- hypin-app/routes/artists.js
+++ hypin-app/routes/artists.js
@@ -6,10 +6,6 @@ const artistsCtrl = require('../controllers/artists');
 	
 // GET /artists/new
 router.get('/new', artistsCtrl.new);
-// GET /artist/releases/
-router.get('/new',)
-// GET /artist/releases/:id (show functionality) MUST be below new route
-
 // POST /artists/new
 router.post('/new',artistsCtrl.query);
 // POST /artists
