ab3a62e39fdf57a9244fba52f26805ecd3b2ff6e
redroguea1 <macdickerson@gmail.com>
2023-03-28 19:46:10 -0700

    updating routes for release page

--- hypin-app/routes/releases.js
+++ hypin-app/routes/releases.js
@@ -6,4 +6,8 @@ const fetch = require('node-fetch');
 
 const releasesCtrl = require('../controllers/releases');
 
+// WHAT getting a showpage to render for our releases based on the ID 
+router.get("/:id/show", releasesCtrl.show)
+//now go build the show ctrlr
+
 module.exports = router;
