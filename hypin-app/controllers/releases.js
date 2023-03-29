da07b9e41aad9f7a9beb5d172cefdffec3b77578
redroguea1 <macdickerson@gmail.com>
2023-03-28 20:30:33 -0700

    continuing show building

--- hypin-app/controllers/releases.js
+++ hypin-app/controllers/releases.js
@@ -7,11 +7,13 @@ const fetch = require('node-fetch');
 const Artist = require("../models/artist")
 
 async function show(req, res) {
-    // //goal: populate the releases field of the artist doc
-    // const artist = await Artist.findById(req.params.id);
-    // //Do i need to populate here?
-    // artist.releases.find( realease => realease === "" )
-    // res.render(':id/show', { title:`${artist.name}`, artist })
+    //match the release id to the number that in my file
+    //goal: populate the releases field of the artist doc
+    console.log(req);
+    const artist = await Artist.findById(req.params.id);
+    console.log(artist);
+    //const release = artist.releases.find( release => release.title === "");
+    res.render(':id/show', { title:`${artist.name}`, release })
 }
 
 
--- hypin-app/routes/releases.js
+++ hypin-app/routes/releases.js
@@ -7,7 +7,7 @@ const fetch = require('node-fetch');
 const releasesCtrl = require('../controllers/releases');
 
 // WHAT getting a showpage to render for our releases based on the ID 
-router.get("/:id/show", releasesCtrl.show)
+router.get("/:id", releasesCtrl.show)
 //now go build the show ctrlr
 
 module.exports = router;
