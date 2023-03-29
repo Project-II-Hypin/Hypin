ee779e3f37a60cf1f18cbfb3cac28ef7c4e05929
V3nonix <liam.grubbs@outlook.com>
2023-03-29 10:39:21 -0700

    Seperating review schema from artist schema.

--- hypin-app/models/artist.js
+++ hypin-app/models/artist.js
@@ -1,20 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const reviewSchema = new Schema({
-    content: {
-        type: String,
-        required: true
-    },
-    rating: {
-        type: Number,
-        min: 1,
-        max: 5,
-        default: 3,
-        required: true
-    } 
-});
-
 const releaseSchema = new Schema({
     title: {
         type: String,
@@ -24,7 +10,7 @@ const releaseSchema = new Schema({
     thumb: String,
     artist: String,
     id: Number,
-    reviews: [reviewSchema]
+    reviews: []
 });
 
 const artistSchema = new Schema({
