b588ff4983fcd5f91d3c9a3516fe8c4876d1d8a4
uncannysmile <sprinklingsunshower@gmail.com>
2023-03-28 17:00:20 -0700

    implemented google authetification button

--- hypin-app/routes/releases.js
+++ hypin-app/routes/releases.js
@@ -3,13 +3,13 @@ const router = express.Router();
 // You'll be creating this controller module next
 const releasesCtrl = require('../controllers/releases');
 
-// GET /movies
-router.get('/', releasesCtrl.index);
-// GET /movies/new
-router.get('/new', releasesCtrl.new);
-// GET /movies/:id (show functionality) MUST be below new route
-router.get('/:id', releasesCtrl.show);
-// POST /movies
-router.post('/', releasesCtrl.create);
+// // GET /movies
+// router.get('/', releasesCtrl.index);
+// // GET /movies/new
+// router.get('/new', releasesCtrl.new);
+// // GET /movies/:id (show functionality) MUST be below new route
+// router.get('/:id', releasesCtrl.show);
+// // POST /movies
+// router.post('/', releasesCtrl.create);
 
 module.exports = router;
